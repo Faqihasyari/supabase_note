5146cc96f7f3563375ba28dd5bfe1cac9e08d789
Faqihasyari <faqih8158@gmail.com>
2025-04-25 16:03:18 +0700

    register fix

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,32 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_note/app/routes/app_pages.dart';
 
 class RegisterController extends GetxController {
   RxBool isLoading = false.obs;
   RxBool isHidden = true.obs;
   TextEditingController emailC = TextEditingController();
-  TextEditingController passC= TextEditingController();
+  TextEditingController passC = TextEditingController();
 
   SupabaseClient client = Supabase.instance.client;
 
-  void signup()async{
-    if(emailC.text.isNotEmpty && passC.text.isNotEmpty){
+  void signup() async {
+    if (emailC.text.isNotEmpty && passC.text.isNotEmpty) {
       isLoading.value = true;
       try {
-      AuthResponse res = await client.auth.signUp(email: emailC.text ,password: passC.text);
-      Get.snackbar("Berhasil", "Akun Berhasil Dibuat");
-      } on AuthException catch (e){
+        AuthResponse res =
+            await client.auth.signUp(email: emailC.text, password: passC.text);
+        Get.snackbar("Berhasil", "Akun Berhasil Dibuat");
+        Get.offAllNamed(Routes.HOME);
+      } on AuthException catch (e) {
         Get.snackbar("Gagal", e.message);
         Get.back();
-      } catch (e){
+      } catch (e) {
         Get.snackbar("Error", e.toString());
       } finally {
         isLoading.value = false;
-      } 
-
-      
-      
+      }
+      // Get.defaultDialog(
+      //   barrierDismissible: false,
+      //     title: "BERHASIL REGISTER",
+      //     middleText: "Periksa dan lakukan email konfirmasi",
+      //     actions: [
+      //       OutlinedButton(
+      //           onPressed: () {
+      //             Get.back(); //Buat tutup dialog
+      //             Get.back(); //Kembali ke login
+      //           }, child: Text("Oke Saya Mengerti"))
+      //     ]);
     } else {
       Get.snackbar("Terjadi Kesalahan", "Email dan password belum diisi");
     }
--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,14 @@ import 'app/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
+  Supabase supa = await Supabase.initialize(
       url: 'https://tqadbjxxrddwhtrdnvsi.supabase.co',
       anonKey:
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxYWRianh4cmRkd2h0cmRudnNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0OTcxODMsImV4cCI6MjA2MTA3MzE4M30.XfAKeb3fC-aeCZEqpXDlDYLAiL-MpOMrjpDTpGo2axo');
   runApp(
     GetMaterialApp(
       title: "Supanote",
-      initialRoute: Routes.LOGIN,
+      initialRoute: supa.client.auth.currentUser == null ? Routes.LOGIN : Routes.HOME,
       getPages: AppPages.routes,
     ),
   );
