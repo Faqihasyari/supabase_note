79e2d30447a8629ada225ec87a14cb68ca39d8d2
Faqihasyari <faqih8158@gmail.com>
2025-04-27 22:57:36 +0700

    test3

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -5,16 +5,16 @@ import 'package:get/get.dart';
 import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
-  const ProfileView({super.key});
+  ProfileView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MY PROFILE'),
+        title:  Text('MY PROFILE'),
         centerTitle: true,
       ),
       body: ListView(
-        
+
       )
     );
   }
