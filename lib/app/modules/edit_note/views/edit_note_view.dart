e8e33c9307b2b55aefdef40369f949d3262b1cdc
Faqihasyari <faqih8158@gmail.com>
2025-05-03 20:46:27 +0700

    editNote

--- lib/app/modules/edit_note/views/edit_note_view.dart
+++ lib/app/modules/edit_note/views/edit_note_view.dart
@@ -1,24 +1,61 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:supabase_note/app/data/models/note_model.dart';
 
 import '../controllers/edit_note_controller.dart';
 
 class EditNoteView extends GetView<EditNoteController> {
-  const EditNoteView({super.key});
+  Note note = Get.arguments;
+  EditNoteView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('EditNoteView'),
+        title: const Text('EDIT NOTE'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'EditNoteView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            TextField(
+              controller: controller.titleC,
+              decoration: InputDecoration(
+                labelText: "Title",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: controller.descC,
+              decoration: InputDecoration(
+                labelText: "Descriprion",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Obx(
+              () => ElevatedButton(
+                  onPressed: () async {
+                    if (controller.isLoading.isFalse) {
+                      //eksekusi addNote
+                      bool res = await controller.addNote();
+                      if (res == true) {
+                        await homeC.getAllNotes();
+                        Get.back();
+                      }
+                    }
+                  },
+                  child: Text(controller.isLoading.isFalse
+                      ? "ADD NOTE"
+                      : "LOADING.....")),
+            )
+          ],
+        )
     );
   }
 }
